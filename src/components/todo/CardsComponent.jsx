7f741ba08c5b1756197a421353515bf9c17af432
Omr50 <omrab4@gmail.com>
2023-05-01 00:23:25 -0400

    Added Test Study mode with card carousel

--- src/components/todo/CardsComponent.jsx
+++ src/components/todo/CardsComponent.jsx
@@ -8,6 +8,9 @@ function Cards() {
     const authContext = useAuth()
     const username = authContext.username
     const [allCards, setCards] = useState([])
+    const [currCard, setCurrCard] = useState(0);
+
+    const [side, setSide] = useState(true)
     const {setname, id} = useParams()
     const [studyMode, setStudyMode] = useState('view')
     const navigate = useNavigate()
@@ -36,75 +39,102 @@ function Cards() {
     }
 
     useEffect(() =>getCards(username, id), [])
-    return (
-        <div>
-            <br></br>
-            <h1>{setname}</h1>
-            <br></br>
-            <span style={{ 'width':'20px', 'backgroundColor':'orange', 'color':'white', 'padding':'10px', borderRadius:'5px', 'cursor':'pointer', 'margin':'5px', 'fontSize':'20px'}} onClick={()=>{navigate(`/card-form/-1/${setname}/${id}`)}}>Add Card</span>
-            <span style={{ 'width':'20px', 'backgroundColor':'#8ca765', 'color':'white', 'padding':'10px', borderRadius:'5px', 'cursor':'pointer', 'margin':'5px', 'fontSize':'20px'}}>Review Set</span>
-            <br></br>
-            <div style={{'display':'flex', 'flex-direction':'row', 'justifyContent':'center'}}>
-                <hr style={{'width':'75%', 'alignContent':'center'}}></hr>
-            </div>
-            <div style={{'display':'flex','justifyContent':'center'}}>
-                <Table striped bordered hover style={{"width":'80%', 'justifyContent':'center'}}>
-                    <thead>
-                        <tr>
-                        <th></th>
-                        <th></th>
-                        <th>Word</th>
-                        <th>Definition</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {allCards.map((card)=> (
-                        <tr key={card.id}>
-                            <td>
-                            <Button variant="danger" onClick={(event)=>{deleteCard(card.id); event.stopPropagation();}}>&#10005;</Button>
-                            </td>
-                            <td>
-                            <Button variant="info" style={{'color':'white'}} onClick={()=>{navigate(`/card-form/${card.id}/${setname}/${id}`)}}>&#9998;</Button>
-                            </td>
-                            <td style={{'fontSize':'20px'}}>{card.word}</td>
-                            <td style={{'fontSize':'20px'}}>{card.definition}</td>
-                        </tr>
-                        ))}
-                    </tbody>
-                    </Table>
+
+    const decrementCard = () => {
+        if (currCard == 0)
+            setCurrCard(allCards.length-1)
+        else{
+            const newVal = currCard - 1
+            setCurrCard(newVal)
+        }
+    }
+
+    const incrementCard = () => {
+        if (currCard == allCards.length-1)
+            setCurrCard(0)
+        else{
+            const newVal = currCard + 1
+            setCurrCard(newVal)
+        }
+    }
+
+    if (studyMode == 'view')
+        return (
+            <div>
+                <br></br>
+                <h1>{setname}</h1>
+                <br></br>
+                <span style={{ 'width':'20px', 'backgroundColor':'orange', 'color':'white', 'padding':'10px', borderRadius:'5px', 'cursor':'pointer', 'margin':'5px', 'fontSize':'20px'}} onClick={()=>{navigate(`/card-form/-1/${setname}/${id}`)}}>Add Card</span>
+                <span style={{ 'width':'20px', 'backgroundColor':'#8ca765', 'color':'white', 'padding':'10px', borderRadius:'5px', 'cursor':'pointer', 'margin':'5px', 'fontSize':'20px'}} onClick={()=>{setStudyMode('test')}}>Review Set</span>
+                <br></br>
+                <div style={{'display':'flex', 'flex-direction':'row', 'justifyContent':'center'}}>
+                    <hr style={{'width':'75%', 'alignContent':'center'}}></hr>
                 </div>
-
-            {/* <div style={{'display':'flex', 'flex-direction':'row', 'justifyContent':'center', 'minHeight':'200px'}}>
-
-                    <Card className="custom-card3 edit-card" style={{'width':'fitContent', 'height':'200px'}}>
-                        <Card.Body style={{"color": "black !important"}}>
-                            <div style={{'fontSize':'30px'}} id="x-button" onClick={(event)=>{deleteCard(card.id); event.stopPropagation();}}>&#10005;</div>
-                            <hr></hr>
-                            <div style={{'fontSize':'30px'}} id="edit-button">&#9998;</div>
-                        </Card.Body>
-                    </Card>
-
-                    <Card className="custom-card">
-                        <Card.Body>
-                            <Card.Title><br></br></Card.Title>
-                            <Card.Text className="card-title" style={{paddingTop:'0px'}}>{card.word}</Card.Text>
-                            </Card.Body>
-                            </Card>
-        
-                            <Card className="custom-card" key={card.id}>
-                                <Card.Body>
-                                    <Card.Title><br></br></Card.Title>
-                                    <Card.Text className="card-title" style={{paddingTop:'0px'}}>{card.definition}</Card.Text>
-                                </Card.Body>
-                            </Card>
-        
-                        </div>
-                        <div style={{'display':'flex', 'flex-direction':'row', 'justifyContent':'center'}}>
-                            <hr style={{'width':'75%', 'alignContent':'center'}}></hr>
-                        </div>  */}
-            
+                <div style={{'display':'flex','justifyContent':'center'}}>
+                    <Table striped bordered hover style={{"width":'80%', 'justifyContent':'center'}}>
+                        <thead>
+                            <tr>
+                            <th></th>
+                            <th></th>
+                            <th>Word</th>
+                            <th>Definition</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {allCards.map((card)=> (
+                            <tr key={card.id}>
+                                <td>
+                                <Button variant="danger" onClick={(event)=>{deleteCard(card.id); event.stopPropagation();}}>&#10005;</Button>
+                                </td>
+                                <td>
+                                <Button variant="info" style={{'color':'white'}} onClick={()=>{navigate(`/card-form/${card.id}/${setname}/${id}`)}}>&#9998;</Button>
+                                </td>
+                                <td style={{'fontSize':'20px'}}>{card.word}</td>
+                                <td style={{'fontSize':'20px'}}>{card.definition}</td>
+                            </tr>
+                            ))}
+                        </tbody>
+                        </Table>
+                    </div>    
     </div>
     )
+    else
+        return (
+            <div>
+                <br></br>
+                <h1>Test Mode</h1>
+                <div style={{'display':'flex', 'flexDirection':'row', 'justifyContent':'center', 'alignItems':'center', 'paddingTop':'100px'}}>
+                    <span style={{'fontSize':'40px', 'cursor':'pointer'}} onClick={decrementCard}> &#x3c; </span>
+                    <Card className="custom-card" onClick={()=>{setSide(!side)}} style={{'minHeight':'250px'}}>
+                        <h9>{currCard+1}/{allCards.length}</h9>
+                        <Card.Body>
+                            <Card.Title><br></br></Card.Title>
+                            {side ? 
+                            <Card.Text style={{paddingTop:'0px', 'fontSize':'40px'}}>{allCards[currCard].word}</Card.Text>
+                            :
+                            <Card.Text style={{paddingTop:'0px', 'fontSize':'40px'}}>{allCards[currCard].definition}</Card.Text>
+                            }   
+                        </Card.Body>
+                    </Card>
+                    <span style={{'fontSize':'40px', 'cursor':'pointer'}}  onClick={incrementCard}> &#x3e; </span>
+
+
+                    {/* <Card className="custom-card" key={card.id}>
+                        <Card.Body>
+                            <Card.Title><br></br></Card.Title>
+                            <Card.Text className="card-title" style={{paddingTop:'0px'}}>{card.definition}</Card.Text>
+                        </Card.Body>
+                    </Card> */}
+
+                </div>
+
+                <br></br>
+                <div>
+                    <h6 className="text-secondary">Click the card to see the definition. Go to next or previous card with arrows.</h6>
+                    <Button variant="warning m-2" onClick={()=>{setStudyMode('view'); setCurrCard(0)}}>Exit</Button>
+                </div>
+            </div>
+        )
 }
 
 export default Cards
--- src/components/todo/HeaderComponent.jsx
+++ src/components/todo/HeaderComponent.jsx
@@ -11,19 +11,19 @@ function HeaderComponent() {
     return (
         <Navbar bg="light" expand="lg" style={{padding:"10px"}}>
         <Link to="/" className='nav-link'>
-            <Navbar.Brand className='ms-2 fs-2 fw-bold text-black'>Study</Navbar.Brand>
+            <Navbar.Brand className='ms-2 fs-1 fw-bold' style={{'color':'#4255ff !important'}}>Study</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-          <Link to="/todos" className='nav-link'>Todos</Link>
-          <Link to="/flashcards" className='nav-link'>Flashcards</Link>
+          <Link to="/todos" className='nav-link nav-link2 fs-5'>Todos</Link>
+          <Link to="/flashcards" className='nav-link nav-link2 fs-5'>Flashcards</Link>
             {/* <Nav.Link href="#contact">Contact</Nav.Link> */}
           </Nav>
           <Nav className="auth-buttons">
-            {!isAuthenticated && <Link to="/login" className='nav-link'>Log In</Link>}
-            {!isAuthenticated && <Link to="/signup" className='nav-link'>Sign Up</Link>}
-            {isAuthenticated && <Link to='/logout' className='nav-link' onClick={logout}>Log Out</Link>}
+            {!isAuthenticated && <Link to="/login" className='nav-link nav-link2 fs-5'>Log In</Link>}
+            {!isAuthenticated && <Link to="/signup" className='nav-link nav-link2 fs-5'>Sign Up</Link>}
+            {isAuthenticated && <Link to='/logout' className='nav-link nav-link2 fs-5' onClick={logout}>Log Out</Link>}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
